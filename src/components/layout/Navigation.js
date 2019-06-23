ed6a1ea0a0c6e3a5ce0c391682cd3b1da7c44dce
Wanda <doubleuevans@gmail.com>
2019-06-23 18:43:11 -0500

    cleaned up code, change classes to func components

--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -1,26 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-export class Navigation extends Component {
-    static defaultProps = {
-        title: "Github Finder dP",
-        icon: "fas fa-angle-double-right"
-    };
+const Navigation = ({ icon, title }) => {
+    return (
+        <nav className='navbar bg-primary'>
+            <h1>
+                <i className={icon} /> {title}
+            </h1>
+        </nav>
+    );
+};
 
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        icon: PropTypes.string.isRequired
-    };
+Navigation.defaultProps = {
+    title: "Github Finder dP",
+    icon: "fas fa-angle-double-right"
+};
 
-    render() {
-        return (
-            <nav className='navbar bg-primary'>
-                <h1>
-                    <i className={this.props.icon} /> {this.props.title}
-                </h1>
-            </nav>
-        );
-    }
-}
+Navigation.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+};
 
 export default Navigation;
