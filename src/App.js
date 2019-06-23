10dc9d1a526074e087dc99d9b923185d5ce48d61
Wanda <doubleuevans@gmail.com>
2019-06-23 14:18:47 -0500

    init commit, cra cleanup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+    //lifecyle method
+    render() {
+        return (
+            <div className='App'>
+                <h1>stand in</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
