dc1cc7dd27a3fe0a3f313b7cde947916efbfc684
Wanda <doubleuevans@gmail.com>
2019-06-23 19:15:13 -0500

    added axios, http reqs, and updated state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React, { Component } from "react";
 import Navigation from "./components/layout/Navigation";
 import Users from "./components/users/Users";
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
+    state = {
+        users: [],
+        loading: false
+    };
+    async componentDidMount() {
+        this.setState({ loading: true });
+
+        const res = await axios.get("https://api.github.com/users");
+
+        //after the response/res
+        this.setState({ users: res.data, loading: false });
+
+        console.log(res.data);
+    }
+
     render() {
         return (
             <div className='App'>
                 <Navigation title='Github Finder' icon='fab fa-github' />
                 <div className='container'>
-                    <Users />
+                    <Users
+                        loading={this.state.loading}
+                        users={this.state.users}
+                    />
                 </div>
             </div>
         );
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -11,13 +11,13 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
                 style={{
                     width: "60px"
                 }}
-            />{" "}
+            />
             <h3> {login} </h3>
             <div>
                 <a href={html_url} className='btn btn-dark btn-sm my-1'>
-                    More{" "}
-                </a>{" "}
-            </div>{" "}
+                    More
+                </a>
+            </div>
         </div>
     );
 };
